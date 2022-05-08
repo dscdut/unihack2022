025ea5316ba75939efdc3dd3138cf4facf211ef4
nt-myduyen <myduyen14125@gmail.com>
2022-05-08 15:57:38 +0700

    feat: btn register

--- assets/main.js
+++ assets/main.js
@@ -37,8 +37,10 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("remain-day").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("min").innerHTML = minutes;
+  document.getElementById("sec").innerHTML = seconds;
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("remain-day").innerHTML = "EXPIRED";
