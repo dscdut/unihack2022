6fd972ba8dfdc799521ff1fa2729513f6d89b48f
nt-myduyen <myduyen14125@gmail.com>
2022-06-01 00:30:37 +0700

    update timeline

--- assets/main.js
+++ assets/main.js
@@ -31,7 +31,7 @@ reszieScreen(size);
 size.addEventListener("change", reszieScreen);
 
 // Set the date we're counting down to
-var countDownDate = new Date("May 24, 2022 23:59:59").getTime();
+var countDownDate = new Date("July 14, 2022 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
